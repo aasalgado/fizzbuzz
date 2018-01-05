f585e038a7d6003b02f32de879c7b8a4d6f38b01
Anthony Salgado <anthonyasalgado@gmail.com>
2018-01-05 09:12:43 -0800

    added loop to check until user input is an integer

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,4 +1,8 @@
 def fizz_buzz(number)
+    until number.is_a?(Fixnum) do
+        print "Please enter a number: "
+        number = Integer(gets) 
+    end
     result = ''
     result += "Fizz" if (number % 3).zero?
     result += "Buzz" if (number % 5).zero?
