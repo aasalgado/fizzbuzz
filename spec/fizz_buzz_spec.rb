5246b46e0bf2577db214a92cecdc7ff4fcab345d
Anthony Salgado <anthonyasalgado@gmail.com>
2018-01-05 08:03:00 -0800

    created test file and first rspec test passed

---
+++ spec/fizz_buzz_spec.rb
@@ -0,0 +1,8 @@
+# test file
+require './lib/fizz_buzz'  #will look for and test the source file in the lib folder, called fizz_buzz.rb
+
+describe 'fizz_buzz' do
+    it 'returns 1 if number is 1' do
+        expect(fizz_buzz(1)).to eq 1
+    end
+end
